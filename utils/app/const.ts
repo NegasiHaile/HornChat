82c0a5b6d7a4d90d2e6a8fef3868d5bdf39ee2c5
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2023-08-28 00:55:08 +0300

    Fixed: Some texts localization issue

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,7 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "The user's input appears to have been translated into English using a translation system, which could potentially introduce mistakes. Do your best to understand and respond accordingly to the request. Respond using markdown.";
+// "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
